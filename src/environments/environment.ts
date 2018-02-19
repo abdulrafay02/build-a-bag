5aada244b0dafef351f51b1f74b7b918e5972ed0
abdulrafay <abdulrafay_02@live.com>
2018-02-20 02:17:04 +0500

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@
 export const environment = {
   production: false,
   amazonCredentials: {
-    awsId: 'AKIAI73KEPDOOHILE2PA',
-    secret: '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',
-    affliateId: 'Emedpack-20'
+    awsId: 'AKIAI7VTNJBUMESZP2SA',
+    secret: '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',
+    affliateId: 'emedpack02-20'
   }
 };
